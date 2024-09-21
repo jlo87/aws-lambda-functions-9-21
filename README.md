183152829a2bf45d86e2ee251020bd13f2d6cddc
Jonathan Lo <jonlo1987@gmail.com>
2024-09-21 15:57:15 -0400

    Initial lambda setup

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # aws-lambda-python-s3
 Build an AWS Lambda function in Python that uses Boto3 to communicate with Amazon S3
+
+## AWS Lambda:
+- Let's you run code without provisioning or managing servers
+- Triggers your code in response to events
+- Scales automatically
+- Provides built-in code monitoring and logging with Amazon CloudWatch
+
+### AWS Lambda use cases:
+- Web apps
+- Backend
+- Data processing
+- Chatbots
+- Amazon Alexa
+- IT automation
+
+## Steps to setup env:
+- Build a virtual environment
+`python3 -m venv ~/.venv`
+`source ~/.venv/bin/activate`
+
+- Install libraries with the Makefile
+`make install`
---
+++ buckets.py
@@ -0,0 +1,30 @@
+import boto3
+
+
+# number_of_buckets function displays number of buckets
+def number_of_buckets(num):
+    client = boto3.client('s3')
+    
+    # Call list_buckets function and put into a buckets result
+    buckets = client.list_buckets()
+
+    count = 0
+    for bucket in buckets['Buckets']:
+        print(f"Processing bucket number-[{count}]")
+
+        if count < num:
+            # Keep processing
+            current_bucket = bucket['Name']
+            print(f"Found Bucket: {current_bucket}")
+            # Processing bucket number-[0]
+            # Found Bucket: alfredo-noah
+            # Processing bucket number-[1]
+            # Found Bucket: aws-logs-561744971673-us-east-1
+            # Processing bucket number-[2]
+
+            # Increment count after each loop
+            count += 1
+        else:
+            return
+
+number_of_buckets[2]
